a7192de2deef4628e4a792ccf51d91d91050b992
caitlinsweeneyuu <sweeney-c33@ulster.ac.uk>
2025-09-29 15:58:11 +0100

    added q4

--- Week1Practical/Program.cs
+++ Week1Practical/Program.cs
@@ -37,3 +37,14 @@ else
 {
     Console.WriteLine("The number is odd.");
 }
+
+Console.WriteLine("Enter a year to check if it's a leap year:");
+int year = Convert.ToInt32(Console.ReadLine());
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+    Console.WriteLine($"{year} is a leap year.");
+}
+else
+{
+    Console.WriteLine($"{year} is not a leap year.");
+}
