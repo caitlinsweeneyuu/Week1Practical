18eb29f6d9a1a9a04e4e95780d0e257c30fbfb71
caitlinsweeneyuu <sweeney-c33@ulster.ac.uk>
2025-09-29 15:45:51 +0100

    added q1.

--- Week1Practical/Program.cs
+++ Week1Practical/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+int ageInFiveYears = age + 5;
+
+Console.WriteLine($"Hello, {name}. You will be {ageInFiveYears} in 5 years.");
