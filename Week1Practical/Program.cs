17f095ad280b1c2b388abd70b9c62e970b7d3626
caitlinsweeneyuu <sweeney-c33@ulster.ac.uk>
2025-09-29 15:54:47 +0100

    answered q3

--- Week1Practical/Program.cs
+++ Week1Practical/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
 Console.WriteLine("Enter your name:");
 string name = Console.ReadLine();
 Console.WriteLine("Enter your age:");
@@ -22,4 +24,16 @@ else if (age > 19 && age < 110){
 else
 {
     Console.WriteLine("Invalid age entered.");
+}
+
+Console.WriteLine("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("The number is even.");
+}
+else
+{
+    Console.WriteLine("The number is odd.");
 }
