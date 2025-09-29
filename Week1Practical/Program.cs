10cba064e78ac83f1e445df3b6fde3a557b16cb0
caitlinsweeneyuu <sweeney-c33@ulster.ac.uk>
2025-09-29 15:53:01 +0100

    added q2

--- Week1Practical/Program.cs
+++ Week1Practical/Program.cs
@@ -6,4 +6,20 @@ int age = Convert.ToInt32(Console.ReadLine());
 
 int ageInFiveYears = age + 5;
 
-Console.WriteLine($"Hello, {name}. You will be {ageInFiveYears} in 5 years.");
+Console.WriteLine($"Hello, {name}. You will be {ageInFiveYears} in 5 years");
+
+if (age < 13)
+{
+    Console.WriteLine("You are a child.");
+}
+else if ( age < 20 && age >= 13)
+{
+    Console.WriteLine("You are a teenager.");
+}
+else if (age > 19 && age < 110){
+    Console.WriteLine("You are an adult.");
+}
+else
+{
+    Console.WriteLine("Invalid age entered.");
+}
